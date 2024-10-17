b74748a638f20036a5b0b320b00235dc01b31f6a
Devin <devinlainediaz@gmail.com>
2024-10-17 13:56:23 -0400

    Added dependencies

--- server.go
+++ server.go
@@ -8,11 +8,37 @@ import (
 	"github.com/gofiber/fiber/v2" // Import the Fiber framework
 )
 
+func indexHandler(c *fiber.Ctx) error {
+	return c.SendString("Hello")
+}
+func postHandler(c *fiber.Ctx) error {
+	return c.SendString("Hello")
+}
+func putHandler(c *fiber.Ctx) error {
+	return c.SendString("Hello")
+}
+func deleteHandler(c *fiber.Ctx) error {
+	return c.SendString("Hello")
+}
+
 func main() {
-	app := fiber.New() // create new Fiber object & assign to App variable
-	port := os.Getenv("PORT")
+	app := fiber.New() // create new Fiber object & assign to app variable
+
+	// add methods to handle GET, POST, PUT, and DELETE operations for app
+	// + four handler methods that are called whenever someone visits those routes
+	app.Get("/", indexHandler)
+
+	app.Post("/", postHandler)
+
+	app.Put("/update", putHandler)
+
+	app.Delete("/delete", deleteHandler)
+
+	port := os.Getenv("PORT") // check environment variables for PORT
 	if port == "" {
-		port = "3000"
+		port = "3000" // if port doesn't exist, set to 3000
 	}
+	// call app.listen to start HTTP server listening on port
+	// log.Fatalln() to log the output to the console of any errors.
 	log.Fatalln(app.Listen(fmt.Sprintf(":%v", port)))
 }
