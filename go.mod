21fff0b26284e82912f2a0921adc85f533c4a745
Devin <devinlainediaz@gmail.com>
2024-10-17 15:05:57 -0400

    Update server.go and connect to Postgres database

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module f
 
 go 1.23.2
 
-require github.com/gofiber/fiber/v2 v2.52.5
+require (
+	github.com/gofiber/fiber/v2 v2.52.5
+	github.com/lib/pq v1.10.9
+)
 
 require (
 	dario.cat/mergo v1.0.1 // indirect
@@ -19,7 +22,6 @@ require (
 	github.com/gohugoio/hugo v0.134.3 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
-	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
--- server.go
+++ server.go
@@ -1,43 +1,88 @@
+//connStr := "host=localhost user=postgres password=246Trin!trotoluene dbname=todo sslmode=disable"
+
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 
-	"github.com/gofiber/fiber/v2" // Import the Fiber framework
+	"github.com/gofiber/fiber/v2"
+	_ "github.com/lib/pq"
 )
 
-func indexHandler(c *fiber.Ctx) error {
+// handlers accept fiber context object and pointer to DB connection
+func indexHandler(c *fiber.Ctx, db *sql.DB) error {
+	var res string
+	var todos []string
+	rows, err := db.Query("SELECT * FROM todos") // use db object to execute SQL query - returns rows that match queries + errors
+
+	defer rows.Close() // close rows
+
+	if err != nil {
+		log.Fatal(err)
+		c.JSON("An error occurred")
+	}
+	for rows.Next() { // iterate over rows 
+		rows.Scan(&res) // use scan() to assign current row to res
+		todos = append(todos, res) // append value of res to array
+	}
+
+	return c.Render("index", fiber.Map{
+		"Todos": todos, // pass todos array to index view
+	})
+}
+func postHandler(c *fiber.Ctx, db *sql.DB) error {
 	return c.SendString("Hello")
 }
-func postHandler(c *fiber.Ctx) error {
+func putHandler(c *fiber.Ctx, db *sql.DB) error {
 	return c.SendString("Hello")
 }
-func putHandler(c *fiber.Ctx) error {
-	return c.SendString("Hello")
-}
-func deleteHandler(c *fiber.Ctx) error {
+func deleteHandler(c *fiber.Ctx, db *sql.DB) error {
 	return c.SendString("Hello")
 }
 
 func main() {
-	app := fiber.New() // create new Fiber object & assign to app variable
+	connStr := "host=localhost user=postgres password=246Trin!trotoluene dbname=todo sslmode=disable"
 
-	// add methods to handle GET, POST, PUT, and DELETE operations for app
+	// Connect to database
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	/* configure our Fiber app to serve our HTML views */
+	engine := html.New("./views",".html")
+	// create new Fiber object & assign to app variable
+	app := fiber.New(fiber.Config{
+		Views: engine,
+		}) 
+
+	// ROUTES
 	// + four handler methods that are called whenever someone visits those routes
-	app.Get("/", indexHandler)
 
-	app.Post("/", postHandler)
+	app.Get("/", func(c *fiber.Ctx) error {
+		return indexHandler(c, db) //pass the database connection to our handlers so we can use it to execute database queries
+	})
 
-	app.Put("/update", putHandler)
+	app.Post("/", func(c *fiber.Ctx) error {
+		return postHandler(c, db)
+	})
 
-	app.Delete("/delete", deleteHandler)
+	app.Put("/update", func(c *fiber.Ctx) error {
+		return putHandler(c, db)
+	})
+
+	app.Delete("/delete", func(c *fiber.Ctx) error {
+		return deleteHandler(c, db)
+	})
 
 	port := os.Getenv("PORT") // check environment variables for PORT
 	if port == "" {
 		port = "3000" // if port doesn't exist, set to 3000
 	}
+
 	// call app.listen to start HTTP server listening on port
 	// log.Fatalln() to log the output to the console of any errors.
 	log.Fatalln(app.Listen(fmt.Sprintf(":%v", port)))
